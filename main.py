f3800869d0882450e2e301066eb5e6e0676e63f6
Vuk Dukic <vuchina@icloud.com>
2024-08-15 21:09:01 -0700

    cingulate

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ class FinancialCrew:
 if __name__ == "__main__":
     print("## Welcome to Financial Analysis Crew")
     print('-------------------------------')
-    company = "Airship AI"
+    company = "Cingulate Inc"
 
     financial_crew = FinancialCrew(company)
     result = financial_crew.run()
